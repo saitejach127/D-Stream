22ef6061690445d886685492794898a12471f465
saitejach127 <saitejach127@gmail.com>
2019-07-06 20:19:33 +0530

    ipfs added

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 8080, () => { console.log("server is On") })
+// const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
+
+
+app.listen(process.env.PORT || 3000, () => { console.log("server is On") })
